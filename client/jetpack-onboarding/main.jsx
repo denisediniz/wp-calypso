9d7c67ff11de0e2b88edbba6f8f882d354702b06
AnnaMag <anna.mag.kedzierska@gmail.com>
2018-01-16 13:21:02 +0000

    update

--- client/jetpack-onboarding/main.jsx
+++ client/jetpack-onboarding/main.jsx
@@ -6,6 +6,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { compact, get } from 'lodash';
 import { connect } from 'react-redux';
+import { recordTracksEvent } from 'state/analytics/actions';
 
 /**
  * Internal dependencies
@@ -16,7 +17,7 @@ import {
 	JETPACK_ONBOARDING_COMPONENTS as COMPONENTS,
 	JETPACK_ONBOARDING_STEPS as STEPS,
 } from './constants';
-import { getUnconnectedSiteIdBySlug } from 'state/selectors';
+import { getJetpackOnboardingSettings, getUnconnectedSiteIdBySlug } from 'state/selectors';
 
 class JetpackOnboardingMain extends React.PureComponent {
 	static propTypes = {
@@ -30,8 +31,7 @@ class JetpackOnboardingMain extends React.PureComponent {
 	// TODO: Add lifecycle methods to redirect if no siteId
 
 	render() {
-		const { jpUser, siteId, siteSlug, stepName, steps, token } = this.props;
-
+		const { recordTracksEventForJpoSite, siteId, siteSlug, stepName, steps } = this.props;
 		return (
 			<Main className="jetpack-onboarding">
 				<Wizard
@@ -39,35 +39,46 @@ class JetpackOnboardingMain extends React.PureComponent {
 					baseSuffix={ siteSlug }
 					components={ COMPONENTS }
 					hideNavigation={ stepName === STEPS.SUMMARY }
-					jpUser={ jpUser }
+					stepName={ stepName }
 					siteId={ siteId }
 					steps={ steps }
-					stepName={ stepName }
-					token={ token }
+					recordTracksEventForJpoSite={ recordTracksEventForJpoSite }
 				/>
 			</Main>
 		);
 	}
 }
 
-export default connect( ( state, { siteSlug } ) => {
-	const siteId = getUnconnectedSiteIdBySlug( state, siteSlug );
-	// Note: here we can select which steps to display, based on user's input
-	const steps = compact( [
-		STEPS.SITE_TITLE,
-		STEPS.SITE_TYPE,
-		STEPS.HOMEPAGE,
-		STEPS.CONTACT_FORM,
-		STEPS.BUSINESS_ADDRESS,
-		STEPS.WOOCOMMERCE,
-		STEPS.SUMMARY,
-	] );
-
-	return {
-		jpUser: get( state.jetpackOnboarding.credentials, [ siteId, 'userEmail' ], null ),
+export default connect(
+	( state, { siteSlug } ) => {
+		// Note: here we can select which steps to display, based on user's input
+		const siteId = getUnconnectedSiteIdBySlug( state, siteSlug );
+		const settings = getJetpackOnboardingSettings( state, siteId );
+		const isBusiness = get( settings, 'siteType' ) === 'business';
+		const steps = compact( [
+			STEPS.SITE_TITLE,
+			STEPS.SITE_TYPE,
+			STEPS.HOMEPAGE,
+			STEPS.CONTACT_FORM,
+			isBusiness && STEPS.BUSINESS_ADDRESS,
+			isBusiness && STEPS.WOOCOMMERCE,
+			STEPS.SUMMARY,
+		] );
+		return {
+			siteId,
+			steps,
+			siteSlug,
+		};
+	},
+	{ recordTracksEvent },
+	( { siteId, ...stateProps }, { recordTracksEvent: recordTracksEventAction }, ownProps ) => ( {
 		siteId,
-		siteSlug,
-		steps,
-		token: get( state.jetpackOnboarding.credentials, [ siteId, 'token' ], null ),
-	};
-} )( JetpackOnboardingMain );
+		...stateProps,
+		recordTracksEventForJpoSite: event =>
+			recordTracksEventAction( event, {
+				blog_id: siteId,
+				site_id_type: 'jpo',
+			} ),
+		...ownProps,
+	} )
+)( JetpackOnboardingMain );
